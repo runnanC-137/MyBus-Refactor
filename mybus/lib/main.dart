2a0de4b3f6fafbef26052af1b7e6a30ffb340c9a
RuannaC-137 <ruanlons@gmail.com>
2023-04-30 16:48:39 -0300

    iguinorando as chaves do firebase

--- mybus/lib/main.dart
+++ mybus/lib/main.dart
@@ -1,8 +1,16 @@
 import "package:flutter/material.dart";
 import "package:mybus/pages/navegador.dart";
 import "package:mybus/rotas.dart";
+import "firebase_options.dart";
+import "package:firebase_core/firebase_core.dart";
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 final ThemeData defaultTheme = ThemeData(
   primaryColor: Colors.indigo,
